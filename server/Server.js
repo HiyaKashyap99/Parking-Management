00c3574cfed3fad552ffb76e5645a1861770db72
HiyaKashyap99 <hiyakashyap2004@gmail.com>
2024-11-19 07:06:09 +0530

    backend

--- server/Server.js
+++ server/Server.js
@@ -24,7 +24,7 @@ app.use('/api/userAccount', userAccountRoutes); // User account routes
 app.use('/api/parkingSpace', parkingSpaceRoutes); // Parking space routes
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
