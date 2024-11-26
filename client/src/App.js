baf69ea38222abca02f7580cf6995be852821c48
HiyaKashyap99 <hiyakashyap2004@gmail.com>
2024-11-26 09:30:08 +0530

    combined backend and frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Login from'./Components/Login';
+import Login from './Components/Login';
 import Signup from './Components/SignUp';
+import Profile from './Components/Profile';
 import Footer from './Components/Footer';
 import ParkingSpace from './Components/ParkingSpace';
-//import './App.css';
+import Wallet from './Components/Wallet';  // Import the Wallet component
+import Contact from './Components/Contact';  // Import the Contact component
+
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Login />
-      <Signup />  
-      <ParkingSpace /> 
+      <Routes>
+        <Route path="/" element={<ParkingSpace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/parking-space" element={<ParkingSpace />} />
+        <Route path="/wallet" element={<Wallet />} />  {/* Add Wallet route */}
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
